0c16aa82f090b522d6d24cf9abf030a4d40bc717
Qian Peisheng <qian_peisheng@i2r.a-star.edu.sg>
2023-10-06 22:51:46 +0800

    add ema data, model and skeleton of consistency loss

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -3,6 +3,7 @@ from .scannet import ScannetDetectionDataset, ScannetDatasetConfig
 from .scannet_base import ScannetDetectionDataset_base, ScannetDatasetConfig_base
 from .scannet_incremental import ScannetDetectionDataset_incremental, ScannetDatasetConfig_incremental
 from .scannet_SDCoT import ScannetDetectionDataset_SDCoT, ScannetDatasetConfig_SDCoT
+from .scannet_Pseudo_EMA import ScannetDetectionDataset_Pseudo_EMA, ScannetDatasetConfig_Pseudo_EMA
 
 from .sunrgbd import SunrgbdDetectionDataset, SunrgbdDatasetConfig
 
@@ -25,6 +26,11 @@ DATASET_FUNCTIONS_INCREMENTAL = {
 
 DATASET_FUNCTIONS_SDCoT = {
     "scannet": [ScannetDetectionDataset_SDCoT, ScannetDatasetConfig_SDCoT],
+    "sunrgbd": [SunrgbdDetectionDataset, SunrgbdDatasetConfig],
+}
+
+DATASET_FUNCTIONS_Pseudo_EMA = {
+    "scannet": [ScannetDetectionDataset_Pseudo_EMA, ScannetDatasetConfig_Pseudo_EMA],
     "sunrgbd": [SunrgbdDetectionDataset, SunrgbdDatasetConfig],
 }
 
@@ -111,6 +117,37 @@ def build_dataset_SDCoT(args):
     dataset_builder_train = DATASET_FUNCTIONS_SDCoT[args.dataset_name][0]
     dataset_builder_test = DATASET_FUNCTIONS_BASE[args.dataset_name][0]
     dataset_config_train = DATASET_FUNCTIONS_SDCoT[args.dataset_name][1](
+        num_base_class=args.num_base_class, num_novel_class=args.num_novel_class)
+    dataset_config_base = DATASET_FUNCTIONS_BASE[args.dataset_name][1](
+        num_base_class=args.num_base_class)
+    dataset_config_val = DATASET_FUNCTIONS_BASE[args.dataset_name][1](
+        num_base_class=args.num_base_class + args.num_novel_class)
+    # note that the val dataset covers both base and incremental classes
+
+    dataset_dict = {
+        "train": dataset_builder_train(
+            dataset_config_train,
+            split_set="train",
+            root_dir=args.dataset_root_dir,
+            meta_data_dir=args.meta_data_dir,
+            use_color=args.use_color,
+            augment=True
+        ),
+        "test": dataset_builder_test(
+            dataset_config_val,
+            split_set="val",
+            root_dir=args.dataset_root_dir,
+            use_color=args.use_color,
+            augment=False,
+        ),
+    }
+    return dataset_dict, dataset_config_train, dataset_config_val, dataset_config_base
+
+def build_dataset_Pseudo_EMA(args):
+    # TODO The current implementation is not correct. the train dataset should not load base classes labels.
+    dataset_builder_train = ScannetDetectionDataset_Pseudo_EMA[args.dataset_name][0]
+    dataset_builder_test = DATASET_FUNCTIONS_BASE[args.dataset_name][0]
+    dataset_config_train = ScannetDetectionDataset_Pseudo_EMA[args.dataset_name][1](
         num_base_class=args.num_base_class, num_novel_class=args.num_novel_class)
     dataset_config_base = DATASET_FUNCTIONS_BASE[args.dataset_name][1](
         num_base_class=args.num_base_class)

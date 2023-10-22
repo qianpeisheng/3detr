cbbba3764caa8699afa45e01a459baece3d0961e
Qian Peisheng <qian_peisheng@i2r.a-star.edu.sg>
2023-10-22 15:57:57 +0000

    add ema pseudo labels (to debug)

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -4,7 +4,7 @@ from .scannet_base import ScannetDetectionDataset_base, ScannetDatasetConfig_bas
 from .scannet_incremental import ScannetDetectionDataset_incremental, ScannetDatasetConfig_incremental
 from .scannet_SDCoT import ScannetDetectionDataset_SDCoT, ScannetDatasetConfig_SDCoT
 from .scannet_Pseudo_EMA import ScannetDetectionDataset_Pseudo_EMA, ScannetDatasetConfig_Pseudo_EMA
-
+from .scannet_Pseudo_2_source_EMA import ScannetDetectionDataset_Pseudo_2_source_EMA, ScannetDatasetConfig_Pseudo_2_source_EMA
 from .sunrgbd import SunrgbdDetectionDataset, SunrgbdDatasetConfig
 
 # TODO implement scannet_incremental
@@ -31,6 +31,11 @@ DATASET_FUNCTIONS_SDCoT = {
 
 DATASET_FUNCTIONS_Pseudo_EMA = {
     "scannet": [ScannetDetectionDataset_Pseudo_EMA, ScannetDatasetConfig_Pseudo_EMA],
+    "sunrgbd": [SunrgbdDetectionDataset, SunrgbdDatasetConfig],
+}
+
+DATASET_FUNCTIONS_Pseudo_2_source_EMA = {
+    "scannet": [ScannetDetectionDataset_Pseudo_2_source_EMA, ScannetDatasetConfig_Pseudo_2_source_EMA],
     "sunrgbd": [SunrgbdDetectionDataset, SunrgbdDatasetConfig],
 }
 
@@ -163,6 +168,38 @@ def build_dataset_Pseudo_EMA(args):
             meta_data_dir=args.meta_data_dir,
             use_color=args.use_color,
             augment=True
+        ),
+        "test": dataset_builder_test(
+            dataset_config_val,
+            split_set="val",
+            root_dir=args.dataset_root_dir,
+            use_color=args.use_color,
+            augment=False,
+        ),
+    }
+    return dataset_dict, dataset_config_train, dataset_config_val, dataset_config_base
+
+def build_dataset_Pseudo_2_source_EMA(args):
+    # TODO The current implementation is not correct. the train dataset should not load base classes labels.
+    dataset_builder_train = DATASET_FUNCTIONS_Pseudo_2_source_EMA[args.dataset_name][0]
+    dataset_builder_test = DATASET_FUNCTIONS_BASE[args.dataset_name][0]
+    dataset_config_train = DATASET_FUNCTIONS_Pseudo_2_source_EMA[args.dataset_name][1](
+        num_base_class=args.num_base_class, num_novel_class=args.num_novel_class)
+    dataset_config_base = DATASET_FUNCTIONS_BASE[args.dataset_name][1](
+        num_base_class=args.num_base_class)
+    dataset_config_val = DATASET_FUNCTIONS_BASE[args.dataset_name][1](
+        num_base_class=args.num_base_class + args.num_novel_class)
+    # note that the val dataset covers both base and incremental classes
+
+    dataset_dict = {
+        "train": dataset_builder_train(
+            dataset_config_train,
+            split_set="train",
+            root_dir=args.dataset_root_dir,
+            meta_data_dir=args.meta_data_dir,
+            use_color=args.use_color,
+            augment=True,
+            use_ema_pseudo_label=args.use_ema_pseudo_label
         ),
         "test": dataset_builder_test(
             dataset_config_val,
